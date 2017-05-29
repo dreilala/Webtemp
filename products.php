bcc5396f7a26b75b956a0dd808d1aa9f8d25fda2
dreilala <brummbaerig@gmail.com>
2017-05-29 08:56:08 +0200

    Update products.php

--- products.php
+++ products.php
@@ -1,4 +1,11 @@
 <?php
+// connect to database
+include 'config/database.php';
+ 
+// include objects
+include_once "objects/product.php";
+include_once "objects/product_image.php";
+include_once "objects/cart_item.php";
 // set page title
 $page_title="Products";
  
