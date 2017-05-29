7f6b970463e830f80499827f1beac1da6e48fd00
dreilala <brummbaerig@gmail.com>
2017-05-29 08:59:03 +0200

    Update cart_item.php

--- objects/cart_item.php
+++ objects/cart_item.php
@@ -27,3 +27,10 @@ $db = $database->getConnection();
 $product = new Product($db);
 $product_image = new ProductImage($db);
 $cart_item = new CartItem($db);
+// to prevent undefined index notice
+$action = isset($_GET['action']) ? $_GET['action'] : "";
+ 
+// for pagination purposes
+$page = isset($_GET['page']) ? $_GET['page'] : 1; // page is the current page, if there's nothing set, default is page 1
+$records_per_page = 6; // set records or rows of data per page
+$from_record_num = ($records_per_page * $page) - $records_per_page; // calculate for the query LIMIT clause
