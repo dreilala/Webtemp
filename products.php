887e332a70e643efe1fab529b6dc6ca668e139e5
dreilala <brummbaerig@gmail.com>
2017-05-29 08:55:32 +0200

    Update and rename product.php to products.php

---
+++ products.php
@@ -0,0 +1,12 @@
+<?php
+// set page title
+$page_title="Products";
+ 
+// page header html
+include 'layout_head.php';
+ 
+// contents will be here 
+ 
+// layout footer code
+include 'layout_foot.php';
+?>
