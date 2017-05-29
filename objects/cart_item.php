2b3a645edb926856c51b568366cbb0e28327e2d9
dreilala <brummbaerig@gmail.com>
2017-05-29 09:01:43 +0200

    Update cart_item.php

--- objects/cart_item.php
+++ objects/cart_item.php
@@ -56,3 +56,33 @@ else{
         echo "<div class='alert alert-danger'>No products found.</div>";
     echo "</div>";
 }
+// check if a cart item exists
+public function exists(){
+ 
+    // query to count existing cart item
+    $query = "SELECT count(*) FROM " . $this->table_name . " WHERE product_id=:product_id AND user_id=:user_id";
+ 
+    // prepare query statement
+    $stmt = $this->conn->prepare( $query );
+ 
+    // sanitize
+    $this->product_id=htmlspecialchars(strip_tags($this->product_id));
+    $this->user_id=htmlspecialchars(strip_tags($this->user_id));
+ 
+    // bind category id variable
+    $stmt->bindParam(":product_id", $this->product_id);
+    $stmt->bindParam(":user_id", $this->user_id);
+ 
+    // execute query
+    $stmt->execute();
+ 
+    // get row value
+    $rows = $stmt->fetch(PDO::FETCH_NUM);
+ 
+    // return
+    if($rows[0]>0){
+        return true;
+    }
+ 
+    return false;
+}
