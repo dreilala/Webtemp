49f09fe82f48b40c645bba42906b31805f18265f
Dreilala <brummbaerig@gmail.com>
2017-05-29 20:16:07 +0200

    die Images sind noch nicht richtig eingebunden

--- layout_head.php
+++ layout_head.php
@@ -1,35 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- 
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
- 
+
     <title><?php echo isset($page_title) ? $page_title : "The Code of a Ninja"; ?></title>
- 
-    <!-- Bootstrap CSS -->
-    <link href="libs/css/bootstrap/dist/css/bootstrap.css" rel="stylesheet" media="screen">
- 
+
+
+    <!-- Latest compiled and minified CSS -->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+   <!-- Optional theme -->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+   <!-- Latest compiled and minified JavaScript -->
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
- 
+
     <!-- custom css for users -->
     <link href="libs/css/user.css" rel="stylesheet" media="screen">
- 
+
 </head>
 <body>
- 
+
     <?php include 'navigation.php'; ?>
- 
+
     <!-- container -->
     <div class="container">
         <div class="row">
- 
+
         <div class="col-md-12">
             <div class="page-header">
                 <h1><?php echo isset($page_title) ? $page_title : "The Code of a Ninja"; ?></h1>
