c9098fbd37d77781b4cd7f2025cb0eb6602ef874
dreilala <brummbaerig@gmail.com>
2017-05-29 09:08:02 +0200

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,3 +1,18 @@
+<script>
+$(document).ready(function(){
+    // add to cart button listener
+    $('.add-to-cart-form').on('submit', function(){
+ 
+        // info is in the table / single product layout
+        var id = $(this).find('.product-id').text();
+        var quantity = $(this).find('.cart-quantity').val();
+ 
+        // redirect to add_to_cart.php, with parameter values to process the request
+        window.location.href = "add_to_cart.php?id=" + id + "&quantity=" + quantity;
+        return false;
+    });
+});
+</script>
 <?php
 // parameters
 $product_id = isset($_GET['id']) ? $_GET['id'] : "";
